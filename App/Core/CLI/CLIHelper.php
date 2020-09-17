af1d41c678df2625cac55ffe42319bb6abee51b4
endriu3314 <andrei.ar1313@gmail.com>
2020-09-17 17:41:50 +0300

    Create CLI Core

---
+++ App/Core/CLI/CLIHelper.php
@@ -0,0 +1,27 @@
+<?php
+
+
+namespace App\Core\CLI;
+
+
+class CLIHelper
+{
+    public function out($message)
+    {
+        echo $message;
+    }
+
+    public function newline($count = 1)
+    {
+        for ($counter = 0; $counter < $count; $counter++) {
+            $this->out("\n");
+        }
+    }
+
+    public function display($message, $foreground_color = null, $background_color = null)
+    {
+        $colors = new Colors();
+        $message = $colors->getColoredString($message, $foreground_color, $background_color);
+        $this->out($message);
+    }
+}
---
+++ App/Core/CLI/CommandController.php
@@ -0,0 +1,22 @@
+<?php
+
+
+namespace App\Core\CLI;
+
+
+abstract class CommandController
+{
+    protected $app;
+
+    abstract public function run($argv);
+
+    public function __construct(CLI $app)
+    {
+        $this->app = $app;
+    }
+
+    protected function getApp()
+    {
+        return $this->app;
+    }
+}
