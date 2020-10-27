aa5fdcc32c76a6d16b44aed2982cfc776b990051
Andrei Croitoru <a.croitoru3@icloud.com>
2020-10-27 12:50:14 +0200

    Modify the router

--- App/Core/Router/Router.php
+++ App/Core/Router/Router.php
@@ -2,69 +2,49 @@
 
 namespace App\Core\Router;
 
-use Exception;
+use App\Core\Config;
 
 class Router
 {
-    private $request;
+    private $routes;
 
-    public function __construct(IRequest $request)
+    public function __construct(Config $routes)
     {
-        $this->request = $request;
-    }
+        $this->routes = [];
 
-    public function __call($name, $args)
-    {
-        list($route, $method) = $args;
+        $routesData = $routes->getData();
 
-        $this->{strtolower($name)}[$this->formatRoute($route)] = $method;
-    }
+        foreach ($routesData["routes"] as $route) {
+            $url = $route["url"];
+            $method = $route["method"];
+            $view = $route["view"];
 
-    private function formatRoute($route)
-    {
-        $result = rtrim($route, '/');
-        if ($result === '') {
-            return '/';
-        }
-        return $result;
-    }
+            $model = "";
+            $controller = "";
+            $action = "";
 
-    private function defaultRequestHandler()
-    {
-        header("{$this->request->serverProtocol} 404 Not Found");
-        require VIEW . "404.php";
-    }
-
-    public function resolve()
-    {
-        try {
-            $methodDictionary = $this->{strtolower($this->request->requestMethod)};
-            $formattedRoute = $this->formatRoute($this->request->requestUri);
-            $method = $methodDictionary[$formattedRoute];
-
-            if (DEBUG) {
-                echo "<div style='font-size: 1rem; display: inline-block; background-color: #f3f3f3; border: 1px solid #ddd; min-width: 230px; padding: 1rem 1rem 1rem 1rem; font-family: monospace;'>";
-                echo "<span style='font-weight: bold; color: #222222; font-size: 2rem; width: 100%'>Link trace</span>" . "<br />";
-                //echo "<span style='font-weight: bold; color: #5c5c5c'>MethodDictionary: </span>"; print_r($methodDictionary); echo "<br />";
-                echo "<span style='font-weight: bold; color: #5c5c5c'>Method: </span>" . $this->request->requestMethod . "<br />";
-                echo "<span style='font-weight: bold; color: #5c5c5c'>Original Route: </span>" . $this->request->requestUri . "<br />";
-                echo "<span style='font-weight: bold; color: #5c5c5c'>Method: </span>"; print_r($method); echo "<br />";
-                echo "</div>";
+            if (isset($route["model"])) {
+                $model = $route["model"];
             }
 
-            if (is_null($method)) {
-                $this->defaultRequestHandler();
-                return;
+            if (isset($route["controller"])) {
+                $controller = $route["controller"];
+
+                if (isset($route["action"])) {
+                    $action = $route["action"];
+                }
             }
-            echo call_user_func_array($method, array($this->request));
 
-        } catch (Exception $e) {
-
+            $this->routes[$method][$url] = new BaseRoute($view, $model, $controller, $action);
         }
     }
 
-    public function __destruct()
+    public function match(string $request, string $requestMethod)
     {
-        $this->resolve();
+        if (isset($this->routes[$requestMethod][$request])) {
+            return $this->routes[$requestMethod][$request];
+        }
+
+        return null;
     }
 }
