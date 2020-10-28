10c76eebcabd719aaef76e50fe8ded5f8de7a5cf
Andrei Croitoru <andrei.ar1313@gmail.com>
2020-10-28 13:49:52 +0200

    Feature/template engine (#15)
    * Create template class
    
    * Create cache folder, used by template engine
    
    * Remove old views
    
    * Modify Core to work with the new template engine
    
    Modify Application - not calling View anymore
    Modify Controller - remove buildView function
    Modify Router - remove View parameters
    
    * Create view example

--- App/Http/Controllers/HomeController.php
+++ App/Http/Controllers/HomeController.php
@@ -3,28 +3,15 @@
 namespace App\Http\Controllers;
 
 use App\Core\Controller;
-use App\Http\Model\Post;
+use App\Core\Template;
 
 class HomeController extends Controller
 {
-    public function index() {
-        $this->buildView([
-            VIEW . "homeController" . DIRECTORY_SEPARATOR . "index.php"
-        ]);
-    }
-
-    public function exampleOne() {
-        require VIEW . "layout/" . "header.php";
-        require VIEW . "home/" . "example_one.php";
-        require VIEW . "layout/" . "footer.php";
-    }
-
-    public function post() {
-        $Post = new Post();
-        $posts = $Post->getAll();
-
-        $this->buildView([
-            VIEW . "books" . DIRECTORY_SEPARATOR . "index.php"
+    public function test()
+    {
+        Template::view('home.html', [
+            'title' => 'Home Page',
+            'colors' => ['red', 'blue', 'green']
         ]);
     }
 }
