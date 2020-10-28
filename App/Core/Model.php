751e422f9e0af02942867707393da000a7f7f9cd
Andrei Croitoru <a.croitoru3@icloud.com>
2020-10-28 20:54:41 +0200

    WIP: Model with ORM
    Save function works (implies insert and update)

--- App/Core/Model.php
+++ App/Core/Model.php
@@ -5,45 +5,219 @@ namespace App\Core;
 use App\Helpers\DebugPDO;
 use PDO;
 
-class Model
+abstract class Model
 {
-    /**
-     * @var null Database Connection
-     */
-    public $db = null;
+    private static $db = null;
 
-    /**
-     * Whenever model is created, open a database connection.
-     */
-    function __construct()
+    private static $tableName;
+
+    public function __construct()
     {
         try {
-            self::openDatabaseConnection();
+            self::$db = self::openDatabaseConnection();
         } catch (\PDOException $e) {
             exit('Database connection could not be established.');
         }
     }
 
-    /**
-     * Open the database connection with the credentials from application/config/config.php
-     */
-    private function openDatabaseConnection()
+    static private function openDatabaseConnection()
     {
-        // set the (optional) options of the PDO connection. in this case, we set the fetch mode to
-        // "objects", which means all results will be objects, like this: $result->user_name !
-        // For example, fetch mode FETCH_ASSOC would return results like this: $result["user_name] !
-        // @see http://www.php.net/manual/en/pdostatement.fetch.php
         $options = array(PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ, PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING);
 
-        // setting the encoding is different when using PostgreSQL
         if (DB_TYPE == "pgsql") {
             $databaseEncodingenc = " options='--client_encoding=" . DB_CHARSET . "'";
         } else {
             $databaseEncodingenc = "; charset=" . DB_CHARSET;
         }
 
-        // generate a database connection, using the PDO connector
-        // @see http://net.tutsplus.com/tutorials/php/why-you-should-be-using-phps-pdo-for-database-access/
-        $this->db = new PDO(DB_TYPE . ':host=' . DB_HOST . ';dbname=' . DB_NAME . $databaseEncodingenc, DB_USER, DB_PASS, $options);
+        return new PDO(DB_TYPE . ':host=' . DB_HOST . ';dbname=' . DB_NAME . $databaseEncodingenc, DB_USER, DB_PASS, $options);
+    }
+
+    private static function getFieldNames(\ReflectionClass $class): array
+    {
+        $fields = [];
+
+        foreach ($class->getProperties(\ReflectionProperty::IS_PUBLIC) as $field) {
+            $fields[] = $field->getName();
+        }
+
+        return $fields;
+    }
+
+    private static function getFieldNamesString(\ReflectionClass $class): string
+    {
+        return implode(",", self::getFieldNames($class));
+    }
+
+    private function getFields(\ReflectionClass $class): array
+    {
+        $fields = [];
+
+        foreach ($class->getProperties(\ReflectionProperty::IS_PUBLIC) as $field) {
+            $fieldName = $field->getName();
+
+            if ($this->{$fieldName} != "")
+                $fields[$fieldName] = $this->{$fieldName};
+        }
+
+        return $fields;
+    }
+
+    private static function getTableName(\ReflectionClass $class): string
+    {
+        return $class->getStaticPropertyValue('tableName');
+    }
+
+    public function insert()
+    {
+        $class = new \ReflectionClass($this);
+        $tableName = $this->getTableName($class);
+
+        $propsToImplode = $this->getFields($class);
+
+        $tableFieldsString = implode(",", array_keys($propsToImplode));
+        $tableValuesString = implode(",", array_map(function ($value) {
+            return "'" . $value . "'";
+        }, $propsToImplode));
+
+        $sqlQuery = "INSERT INTO " . $tableName . "(" . $tableFieldsString . ")" . " VALUES " . "(" . $tableValuesString . ")" . ";";
+
+        echo $sqlQuery;
+        //$this->db->exec($sqlQuery);
+
+        //$this->id = (int)$this->db->lastInsertId();
+    }
+
+    public function update()
+    {
+        $class = new \ReflectionClass($this);
+        $tableName = $this->getTableName($class);
+
+        $propsToImplode = $this->getFields($class);
+
+        foreach ($propsToImplode as $key => $prop)
+            $propsToImplode[$key] = "'" . $prop . "'";
+
+        $str = '';
+        foreach ($propsToImplode as $key => $item) {
+            $str .= $key . '=' . $item . ',';
+        }
+        $str = rtrim($str, ',');
+
+        $sqlQuery = "UPDATE " . $tableName . " SET " . $str . " WHERE id = " . $this->id;
+
+        echo $sqlQuery;
+    }
+
+    public function save()
+    {
+        if ($this->id > 0) {
+            $this->update();
+        } else {
+            $this->insert();
+        }
+    }
+
+    public static function findd(int $id)
+    {
+        $class = new \ReflectionClass(get_called_class());
+
+        $tableName = self::getTableName($class);
+        $fields = self::getFieldNamesString($class);
+
+        $sql = "SELECT " . $fields . " FROM " . $tableName . " WHERE id = " . $id . ";";
+
+        echo $sql;
+    }
+
+    public static function first()
+    {
+        $class = new \ReflectionClass(get_called_class());
+
+        $tableName = self::getTableName($class);
+        $fields = self::getFieldNamesString($class);
+
+        $sql = "SELECT " . $fields . " FROM " . $tableName . " ORDER BY id ASC LIMIT 1";
+
+        echo $sql;
+    }
+
+    public static function last()
+    {
+        $class = new \ReflectionClass(get_called_class());
+
+        $tableName = self::getTableName($class);
+        $fields = self::getFieldNamesString($class);
+
+        $sql = "SELECT " . $fields . " FROM " . $tableName . " ORDER BY id DESC LIMIT 1";
+
+        echo $sql;
+    }
+
+    private static $sql = "";
+
+    public static function select()
+    {
+        $class = new \ReflectionClass(get_called_class());
+
+        $tableName = self::getTableName($class);
+        $fields = self::getFieldNamesString($class);
+
+        self::$sql = "SELECT " . $fields . " FROM " . $tableName . ";";
+    }
+
+    public static function get()
+    {
+        $class = new \ReflectionClass(get_called_class());
+        return self::$sql;
+    }
+
+    public function find($options = [])
+    {
+        $result = [];
+        $query = '';
+
+        if (is_array($options)) {
+            $whereConditions = [];
+            foreach ($options as $key => $value) {
+                $whereConditions[] = '`' . $key . '` = "' . $value . '"';
+            }
+            $query = " WHERE " . implode(' AND ', $whereConditions);
+        } elseif (is_string($options)) {
+            $query = 'WHERE ' . $options;
+        } else {
+            throw new \Exception('Wrong parameter type of options');
+        }
+
+        echo $query;
+
+        $raw = self::$db->exec($query);
+        foreach ($raw as $rawRow) {
+            $result[] = self::morph($rawRow);
+        }
+
+        return $result;
+    }
+
+    public static function morph(array $object)
+    {
+        $class = new \ReflectionClass(get_called_class());
+
+        $entity = $class->newInstance();
+
+        foreach ($class->getProperties(\ReflectionProperty::IS_PUBLIC) as $prop) {
+            if (isset($object[$prop->getName()])) {
+                $prop->setValue($entity, $object[$prop->getName()]);
+            }
+        }
+
+        $entity->initialize(); // soft magic
+
+        return $entity;
+    }
+
+    protected function sanitize(string $input): string
+    {
+        return htmlspecialchars($input, ENT_QUOTES);
     }
 }
