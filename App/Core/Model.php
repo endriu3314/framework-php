bac864d85cbd2f63e794fba0abacb192ae74c267
Andrei Croitoru <andrei.ar1313@gmail.com>
2020-10-31 16:10:25 +0200

    Feautre/orm (#16)
    * WIP: Model with ORM
    
    Save function works (implies insert and update)
    
    * WIP: Update Post model to new class
    
    Add a test route
    
    * Create a separate database class
    
    * Dummy versions, nothing works in here
    
    * New Model
    
    New base model
    
    Create function implemented
    New parameters for models, primaryKey
    
    * Update Post to new Model
    
    * Update function
    
    * Create delete function
    
    Modify where to work with an array, not only with PK
    Create delete function
    
    * First and last function
    
    * Find by PK
    
    * Select all where
    
    * Remove old model dummy files
    
    * Replace qualifiers with imports
    
    * Clear Home Controller and routes

--- App/Core/Model.php
+++ App/Core/Model.php
@@ -2,48 +2,274 @@
 
 namespace App\Core;
 
-use App\Helpers\DebugPDO;
+use Exception;
 use PDO;
+use ReflectionClass;
+use ReflectionProperty;
 
-class Model
+abstract class Model extends Database
 {
-    /**
-     * @var null Database Connection
-     */
-    public $db = null;
+    private $stmt;
+    private $lastId;
 
-    /**
-     * Whenever model is created, open a database connection.
-     */
-    function __construct()
+    private static $tableName;
+    private static $primaryKey;
+
+    public function __construct()
+    {
+        parent::__construct();
+        self::$tableName = $this->getTableName(new ReflectionClass($this));
+        self::$primaryKey = $this->getPrimaryKey(new ReflectionClass($this));
+    }
+
+    private function getTableName(ReflectionClass $param)
+    {
+        return $param->getStaticPropertyValue("tableName");
+    }
+
+    private function getPrimaryKey(ReflectionClass $param)
     {
         try {
-            self::openDatabaseConnection();
-        } catch (\PDOException $e) {
-            exit('Database connection could not be established.');
+            return $param->getStaticPropertyValue("primaryKey");
+        } catch (Exception $e) {
+            return "id";
         }
     }
 
-    /**
-     * Open the database connection with the credentials from application/config/config.php
-     */
-    private function openDatabaseConnection()
+    private function getFieldsArray(ReflectionClass $param)
     {
-        // set the (optional) options of the PDO connection. in this case, we set the fetch mode to
-        // "objects", which means all results will be objects, like this: $result->user_name !
-        // For example, fetch mode FETCH_ASSOC would return results like this: $result["user_name] !
-        // @see http://www.php.net/manual/en/pdostatement.fetch.php
-        $options = array(PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ, PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING);
+        $fields = [];
 
-        // setting the encoding is different when using PostgreSQL
-        if (DB_TYPE == "pgsql") {
-            $databaseEncodingenc = " options='--client_encoding=" . DB_CHARSET . "'";
-        } else {
-            $databaseEncodingenc = "; charset=" . DB_CHARSET;
+        foreach ($param->getProperties(ReflectionProperty::IS_PUBLIC) as $field) {
+            $fieldName = $field->getName();
+
+            if ($this->{$fieldName} != "")
+                $fields[$fieldName] = $this->{$fieldName};
         }
 
-        // generate a database connection, using the PDO connector
-        // @see http://net.tutsplus.com/tutorials/php/why-you-should-be-using-phps-pdo-for-database-access/
-        $this->db = new PDO(DB_TYPE . ':host=' . DB_HOST . ';dbname=' . DB_NAME . $databaseEncodingenc, DB_USER, DB_PASS, $options);
+        return $fields;
+    }
+
+    private function generateFieldsString($data = [])
+    {
+        $fields = [];
+
+        if (!empty($data)) {
+            foreach ($data as $key => $value) {
+                $fields[] = $key;
+            }
+
+            return implode(",", $fields);
+        }
+
+        return "*";
+    }
+
+    private function generateValuesString($data = [])
+    {
+        $values = [];
+
+        foreach ($data as $key => $value) {
+            $values[] = ":{$key}";
+        }
+
+        return implode(",", $values);
+    }
+
+    private function generateInsertQueryString($data)
+    {
+        $fields = $this->generateFieldsString($data);
+        $values = $this->generateValuesString($data);
+
+        return "INSERT INTO " . self::$tableName . " " . "(" . $fields . ")" . " VALUES " . "(" . $values . ")";
+    }
+
+    private function generateUpdateQueryString($data, $where)
+    {
+        $fields = $this->generateFieldsBindString($data, ',');
+
+        return "UPDATE " . self::$tableName . " SET " . $fields . " " . $where;
+    }
+
+    private function generateDeleteQueryString($where)
+    {
+        return "DELETE FROM " . self::$tableName . " " . $where;
+    }
+
+    private function generateSelectQueryString($data, $where = "", $order = "")
+    {
+        $fields = $this->generateFieldsString($data);
+
+        return "SELECT " . $fields . " FROM " . self::$tableName . " " . $where . " " . $order;
+    }
+
+    private function bindParamsToStmt($params)
+    {
+        foreach ($params as $key => $value) {
+            $type = (is_int($value)) ? PDO::PARAM_INT : PDO::PARAM_STR;
+            $this->stmt->bindValue(":{$key}", $value, $type);
+        }
+    }
+
+    private function generateFieldsBindString($array, $separator)
+    {
+        $param = [];
+
+        foreach ($array as $key => $value) {
+            $param[] = "${key} = :${key}";
+        }
+
+        return implode($separator, $param);
+    }
+
+    private function generateWhereStatement($conditions, $separator = '')
+    {
+        return "WHERE " . $this->generateFieldsBindString($conditions, $separator);
+    }
+
+    private function setPrimaryKeyToReflectionClass($value)
+    {
+        $this->{self::$primaryKey} = $value;
+    }
+
+    private function setFieldsToReflectionClass($array)
+    {
+        foreach ($array as $key => $value) {
+            $this->{$key} = $array[$key];
+        }
+    }
+
+    private function updateLastId()
+    {
+        $id = $this->conn->lastInsertId();
+        $this->lastId = $id ?? $this->lastId;
+    }
+
+    public function lastId()
+    {
+        $this->updateLastId();
+        return $this->lastId;
+    }
+
+    public function first()
+    {
+        $class = new ReflectionClass($this);
+
+        $dataToSelect = $this->getFieldsArray($class);
+
+        $order = " ORDER BY " . self::$primaryKey . " ASC LIMIT 1";
+
+        $this->stmt = $this->conn->prepare($this->generateSelectQueryString($dataToSelect,null,$order));
+        $this->stmt->execute();
+
+        $data = $this->stmt->fetch(PDO::FETCH_ASSOC);
+
+        $this->setFieldsToReflectionClass($data);
+
+        //fallback in case user assigns variable
+        return $this;
+    }
+
+    public function last()
+    {
+        $class = new ReflectionClass($this);
+
+        $dataToSelect = $this->getFieldsArray($class);
+
+        $order = " ORDER BY " . self::$primaryKey . " DESC LIMIT 1";
+
+        $this->stmt = $this->conn->prepare($this->generateSelectQueryString($dataToSelect,null,$order));
+        $this->stmt->execute();
+
+        $data = $this->stmt->fetch(PDO::FETCH_ASSOC);
+
+        $this->setFieldsToReflectionClass($data);
+
+        //fallback in case user assigns variable
+        return $this;
+    }
+
+    public function find($primaryKeyValue)
+    {
+        $class = new ReflectionClass($this);
+
+        $dataToSelect = $this->getFieldsArray($class);
+
+        $where = $this->generateWhereStatement([self::$primaryKey => $primaryKeyValue]);
+
+        $this->stmt = $this->conn->prepare($this->generateSelectQueryString($dataToSelect, $where, null));
+        $this->bindParamsToStmt([self::$primaryKey => $primaryKeyValue]);
+        $this->stmt->execute();
+
+        $data = $this->stmt->fetch(PDO::FETCH_ASSOC);
+
+        $this->setFieldsToReflectionClass($data);
+
+        return $this;
+    }
+
+    public function selectAllWhere($conditions = [], $separator = "OR")
+    {
+        $class = new ReflectionClass($this);
+
+        $dataToselect = $this->getFieldsArray($class);
+
+        $where = $this->generateWhereStatement($conditions, " " . $separator . " ");
+
+        $this->stmt = $this->conn->prepare($this->generateSelectQueryString($dataToselect, $where));
+        $this->bindParamsToStmt($conditions);
+        $this->stmt->execute();
+
+        return $this->stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    public function create()
+    {
+        $class = new ReflectionClass($this);
+
+        $dataToInsert = $this->getFieldsArray($class);
+
+        $this->stmt = $this->conn->prepare($this->generateInsertQueryString($dataToInsert));
+        $this->bindParamsToStmt($dataToInsert);
+
+        $this->stmt->execute();
+
+        $this->setPrimaryKeyToReflectionClass($this->lastId());
+    }
+
+    public function update()
+    {
+        $class = new ReflectionClass($this);
+
+        $dataToInsert = $this->getFieldsArray($class);
+
+        if (!array_key_exists(self::$primaryKey, $dataToInsert))
+            return false;
+
+        $where = $this->generateWhereStatement([self::$primaryKey => $dataToInsert[self::$primaryKey]]);
+        $this->stmt = $this->conn->prepare($this->generateUpdateQueryString($dataToInsert, $where));
+
+        $this->bindParamsToStmt($dataToInsert);
+        $this->stmt->execute();
+
+        return true;
+    }
+
+    public function delete()
+    {
+        $class = new ReflectionClass($this);
+
+        $dataToDelete = $this->getFieldsArray($class);
+
+        if (!array_key_exists(self::$primaryKey, $dataToDelete))
+            return false;
+
+        $where = $this->generateWhereStatement([self::$primaryKey => $dataToDelete[self::$primaryKey]]);
+        $this->stmt = $this->conn->prepare($this->generateDeleteQueryString($where));
+
+        $this->bindParamsToStmt([self::$primaryKey => $dataToDelete[self::$primaryKey]]);
+
+        $this->stmt->execute();
+        return true;
     }
 }
