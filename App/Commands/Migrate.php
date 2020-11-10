8d11d75b261ab01608458d06882d2bc48b742473
Andrei Croitoru <andrei.ar1313@gmail.com>
2020-11-10 22:58:33 +0200

    Fix/multiple migration files (#27)
    * Fix command registery
    
    * Destruct class after use, for re-use in other files
    
    * Modify migrate command to use multiple files for migrations
    
    * Validation required to migrate
    
    * Apply fixes from StyleCI (#28)
    
    Co-authored-by: Andrei Croitoru <endriu3314@users.noreply.github.com>
    
    Co-authored-by: Andrei Croitoru <endriu3314@users.noreply.github.com>

--- App/Commands/Migrate.php
+++ App/Commands/Migrate.php
@@ -3,18 +3,44 @@
 namespace App\Commands;
 
 use App\Core\CLI\CommandController;
-use App\Core\Schema\Schema;
+use App\Helpers\StringHelper;
 
 class Migrate extends CommandController
 {
     public function run($argv)
     {
-        $schema = new Schema();
+        $printer = $this->getApp()->getPrinter();
 
-        $schema->create('users', function ($table) {
-            $table->INT('id')->primaryKey()->autoIncrement()->defaultValue(0)->primaryKey();
-            $table->VARCHAR('username', 144);
-            $table->TEXT('bio')->nullable()->maxValue(200);
-        })->generate();
+        $printer->display('Migrations to run:', 'white');
+
+        $printer->newline(2);
+
+        $migrations = scandir('App/Migrations');
+
+        for ($counter = 2; $counter < count($migrations); $counter++) {
+            $printer->display('- ', 'white');
+            $printer->display(StringHelper::removeExtension($migrations[$counter]), 'green');
+            $printer->newline();
+        }
+
+        $printer->newline();
+        $printer->display('Do you want to run the migrations? ', 'yellow');
+        $printer->display('[yes/no] ', 'green');
+        $printer->display('no ', 'white');
+
+        $handle = fopen('php://stdin', 'r');
+        $line = fgets($handle);
+
+        if (trim($line) != 'yes') {
+            $printer->newline();
+            $printer->display('Aborting!', 'red');
+            exit;
+        }
+
+        fclose($handle);
+
+        for ($counter = 2; $counter < count($migrations); $counter++) {
+            include 'App/Migrations/'.$migrations[$counter];
+        }
     }
 }
