ef47f2f3989e74f0632a737671a67c1048e52f0b
Andrei Croitoru <andrei.ar1313@gmail.com>
2020-11-10 22:57:03 +0200

    Apply fixes from StyleCI (#28)
    Co-authored-by: Andrei Croitoru <endriu3314@users.noreply.github.com>

--- App/Commands/Migrate.php
+++ App/Commands/Migrate.php
@@ -11,36 +11,36 @@ class Migrate extends CommandController
     {
         $printer = $this->getApp()->getPrinter();
 
-        $printer->display("Migrations to run:", "white");
+        $printer->display('Migrations to run:', 'white');
 
         $printer->newline(2);
 
-        $migrations = scandir("App/Migrations");
+        $migrations = scandir('App/Migrations');
 
         for ($counter = 2; $counter < count($migrations); $counter++) {
-            $printer->display("- ", "white");
-            $printer->display(StringHelper::removeExtension($migrations[$counter]), "green");
+            $printer->display('- ', 'white');
+            $printer->display(StringHelper::removeExtension($migrations[$counter]), 'green');
             $printer->newline();
         }
 
         $printer->newline();
-        $printer->display("Do you want to run the migrations? ", "yellow");
-        $printer->display("[yes/no] ", "green");
-        $printer->display("no ", "white");
+        $printer->display('Do you want to run the migrations? ', 'yellow');
+        $printer->display('[yes/no] ', 'green');
+        $printer->display('no ', 'white');
 
-        $handle = fopen("php://stdin", "r");
+        $handle = fopen('php://stdin', 'r');
         $line = fgets($handle);
 
         if (trim($line) != 'yes') {
             $printer->newline();
-            $printer->display("Aborting!", "red");
+            $printer->display('Aborting!', 'red');
             exit;
         }
 
         fclose($handle);
 
         for ($counter = 2; $counter < count($migrations); $counter++) {
-            include "App/Migrations/" . $migrations[$counter];
+            include 'App/Migrations/'.$migrations[$counter];
         }
     }
 }
