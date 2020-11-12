880d03f7ade72326ec212c2272868a9c6fa9366e
Andrei Croitoru <endriu3314@users.noreply.github.com>
2020-11-12 12:41:39 +0000

    Apply fixes from StyleCI

--- App/Core/Exceptions/FileNotFoundException.php
+++ App/Core/Exceptions/FileNotFoundException.php
@@ -1,14 +1,12 @@
 <?php
 
-
 namespace App\Core\Exceptions;
-
 
 use Throwable;
 
 class FileNotFoundException extends \Exception
 {
-    public function __construct($message = "", $code = 0, Throwable $previous = null)
+    public function __construct($message = '', $code = 0, Throwable $previous = null)
     {
         parent::__construct($message, $code, $previous);
     }
