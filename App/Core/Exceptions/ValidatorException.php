dca6a2bed1e057ac64c21e12696017849dc5aca1
Andrei Croitoru <a.croitoru3@icloud.com>
2020-11-12 11:19:16 +0200

    Create validator exception

---
+++ App/Core/Exceptions/ValidatorException.php
@@ -0,0 +1,21 @@
+<?php
+
+
+namespace App\Core\Exceptions;
+
+
+use LogicException;
+use Throwable;
+
+class ValidatorException extends LogicException
+{
+    public function __construct($message = "", $code = 0, Throwable $previous = null)
+    {
+        parent::__construct($message, $code, $previous);
+    }
+
+    public function __toString()
+    {
+        return __CLASS__ . ": [{$this->code}]: {$this->message}\n";
+    }
+}
