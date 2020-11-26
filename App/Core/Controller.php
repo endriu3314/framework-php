51c4dca8e06e18a2a33fec926060726b9b4a0499
Andrei Croitoru <andrei.ar1313@gmail.com>
2020-11-25 23:10:16 -0800

    Feature/validate errors (#66)
    * Add error messages to validator
    
    * Add validate and errors to controller
    
    * Update PHPDoc

--- App/Core/Controller.php
+++ App/Core/Controller.php
@@ -2,6 +2,20 @@
 
 namespace App\Core;
 
-class Controller
+use App\Core\Helpers\Validator;
+
+abstract class Controller
 {
+    public $validateErrors = [];
+
+    public function validate(array $validator): bool
+    {
+        foreach ($validator as $value => $rules) {
+            if (!Validator::is($value, $rules, $this->validateErrors)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
 }
