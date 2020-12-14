af33af78268b62d616e60afed3b4686638930443
Andrei Croitoru <a.croitoru3@icloud.com>
2020-12-14 14:23:17 +0200

    Create a function to get public properties with values

--- App/Core/Helpers/ReflectionHelper.php
+++ App/Core/Helpers/ReflectionHelper.php
@@ -29,6 +29,38 @@ class ReflectionHelper
         return $properties;
     }
 
+    public static function getNonNullPublicProperties($class): array
+    {
+        $reflectionClass = new ReflectionClass($class);
+
+        $properties = [];
+
+        foreach ($reflectionClass->getProperties(ReflectionProperty::IS_PUBLIC) as $property) {
+            $propertyName = $property->getName();
+            $propertyValue = $property->getValue($class);
+
+            if ($propertyValue !== '') {
+                $properties[] = $propertyName;
+            }
+        }
+
+        return $properties;
+    }
+
+    public static function getPublicPropertiesValues($class): array
+    {
+        $reflectionClass = new ReflectionClass($class);
+
+        $properties = [];
+
+        foreach (self::getNonNullPublicProperties($class) as $property) {
+            $propertyValue = $reflectionClass->getProperty($property)->getValue($class);
+            $properties[$property] = $propertyValue;
+        }
+
+        return $properties;
+    }
+
     public static function setFieldsToReflectionClass($class, array $data): void
     {
         $properties = self::getPublicProperties($class);
