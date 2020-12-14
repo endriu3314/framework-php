563706800eb5483dd18627ffb85a6e05fbe96313
Andrei Croitoru <a.croitoru3@icloud.com>
2020-12-14 12:29:14 +0200

    Create ReflectionHelper to work with reflection classes

---
+++ App/Core/Helpers/ReflectionHelper.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Core\Helpers;
+
+use ReflectionClass;
+use ReflectionProperty;
+
+class ReflectionHelper
+{
+    public static function getPrivatePropertyValue($class, string $propertyName): mixed
+    {
+        $reflectionClass = new ReflectionClass($class);
+        $property = $reflectionClass->getProperty($propertyName);
+        $property->setAccessible(true);
+        return $property->getValue($class);
+    }
+
+    public static function getPublicProperties($class): array
+    {
+        $reflectionClass = new ReflectionClass($class);
+
+        $properties = [];
+
+        foreach ($reflectionClass->getProperties(ReflectionProperty::IS_PUBLIC) as $property) {
+            $propertyName = $property->getName();
+            $properties[] = $propertyName;
+        }
+
+        return $properties;
+    }
+
+    public static function setFieldsToReflectionClass($class, array $data): void
+    {
+        $properties = self::getPublicProperties($class);
+        $reflectionClass = new ReflectionClass($class);
+
+        foreach ($properties as $property) {
+            $reflectionClass->getProperty($property)->setValue($class, $data[$property]);
+        }
+    }
+}
